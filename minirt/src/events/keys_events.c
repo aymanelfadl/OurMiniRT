56628ce3d0e885b6b35887503ecd9534ecdd86b5
ayman elfadl <aymanelfadl16@gmail.com>
2025-07-25 18:08:41 +0100

    ok

--- minirt/src/events/keys_events.c
+++ minirt/src/events/keys_events.c
@@ -8,21 +8,15 @@ int key_hook(int keycode, void *scene)
 
     if (keycode == 65307)
         exit(0);
-
-    printf("befor any events:\n");
-    print_debug(s);
-
     if (s->selected_mesh)
     {
         move_object(s, keycode);
         rotate_objects(s, keycode);
         scale_object(s, keycode);
     }
-    rotate_camera(s, keycode);
+    rotate_camera(&s->camera, keycode);
     move_camera(s, keycode);
     render(s);
-    printf("after any events:\n");
-    print_debug(s);
 
     return 0;
 }
--- minirt/src/rendering/ray_color.c
+++ minirt/src/rendering/ray_color.c
@@ -21,11 +21,11 @@ t_vec3 compute_lighting(t_hit h, t_scene *scene)
     to_light = vec3_normalize(to_light);
 
     t_ray shadow_ray;
-    shadow_ray.origin = vec3_add(h.p, vec3_mult(h.n, 1e-6));
+    shadow_ray.origin = vec3_add(h.p, vec3_mult(h.n, 0));
     shadow_ray.direction = to_light;
 
     t_hit hit_p = hit_scene(scene->meshes, &shadow_ray);
-    if (hit_p.t > 1e-6 && hit_p.t < light_distance)
+    if (hit_p.t > 0 && hit_p.t < light_distance)
         return ambient;
      
     /* === Diffuse === */
